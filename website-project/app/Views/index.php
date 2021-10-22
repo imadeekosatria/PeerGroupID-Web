903f5af1603a5e2650a744fdaaccf37cfe4e5431
imadeekosatria <imadeekosatriawiguna@gmail.com>
2021-10-22 22:16:53 +0800

    Update Home Page

--- website-project/app/Views/index.php
+++ website-project/app/Views/index.php
@@ -24,8 +24,8 @@
                     <i class='bx bx-x'></i>
                 </div>
                 <ul class="nav__list">
-                    <li class="nav__item"><a href="index.html" class="nav__link">Home</a></li>
-                    <li class="nav__item"><a href="index.html" class="nav__link">About Us</a></li>
+                    <li class="nav__item"><a href="index.html" class="nav__link">Beranda</a></li>
+                    <li class="nav__item"><a href="index.html" class="nav__link">Siapa Kita</a></li>
                     
                     <li class="nav__item dropdown" >
                         <a href="#" class="nav__link dropdown__link" id="dropdown">Artikel <i class='bx bx-chevron-down dropdown__icon' id="drop-icon"></i></a>
@@ -35,7 +35,7 @@
                             <li class="dropdown__item"><a href="#" class="nav__link">Karir</a></li>
                         </ul>
                     </li>
-
+                    <li class="nav__item"><a href="index.html" class="nav__link">Kegiatan Kami</a></li>
                     <li class="nav__item"><a href="#" class="nav__link">Kontak</a></li>
                 </ul>
             </div>
@@ -53,8 +53,7 @@
                     <a href="" class="button-link kepoin">Kepoin Yuk <i class='bx bx-arrow-back bx-rotate-270' ></i></a>
                 </div>
                 <div class="home__img">
-                    <img class="back" src="/assets/images/Ellipse 1.svg" alt="">
-                    <img class="front" src="/assets/images/FAQs-pana 1.png" alt="">
+                    <img class="front" src="/assets/images/FAQ.svg" alt="">
                 </div>
             </div>
         </section>
@@ -66,7 +65,7 @@
 
             <div class="about__container bd-grid">
                 <div class="about__img">
-                    <img src="/assets/images/Questions-bro 1.svg" alt="">
+                    <img src="/assets/images/Siapakita.svg" alt="">
                 </div>
                 
                 <div>
@@ -81,10 +80,104 @@
             </div>
         </section>
 
-        <!--Artikel-->
-        <section class="artikel section" id="artikel">
+                <!--Artikel-->
+                <section class="artikel section" id="artikel">
             <h2 class="section-title">Artikel</h2>
+            <div class= "artikel-container">
+            <div class=kategori>
+                <h3 class="text-artikel">Self Development</h3>
+            <div class="artikel-content">
+                <h4 class="text-artikel">E-Commerce</h4>
+                <p class="text-isi-artikel">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+                <a href="/" class="selengkapnya-artikel">Selengkapnya<i class='bx bx-arrow-back bx-flip-horizontal' ></i></a>
+            </div>
+            <div class=kategori>
+                <h3 class="text-artikel">Jurusan</h3>
+            <div class="artikel-content">
+                <h4 class="text-artikel">E-Commerce</h4>
+                <p class="text-isi-artikel">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+                <a href="/" class="selengkapnya-artikel">Selengkapnya<i class='bx bx-arrow-back bx-flip-horizontal' ></i></a>
+            </div>
+            <div class=kategori>
+                <h3 class="text-artikel">Karir</h3>
+            <div class="artikel-content">
+                <h4 class="text-artikel">E-Commerce</h4>
+                <p class="text-isi-artikel">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+                <a href="/" class="selengkapnya-artikel">Selengkapnya<i class='bx bx-arrow-back bx-flip-horizontal' ></i></a>
+            </div>
+            </div>
         </section>
+
+        <!-- KEGIATAN KAMI -->
+        <section class="kegiatan-kami section" id="kegiatan-kami">
+        <h2 class="section-title">Kegiatan Kami</h2>
+        <div class="activity-container">
+            <div class="activity">
+            <img src="/assets/images/gambar1.png" class="img-activity" alt="">
+                <div class="activity-description">
+                    <h5 class="title-activity">LOREM IPSUM</h5>
+                    <p class="text-activity">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam</p>
+                    <!-- <a href="" class="button-activity">Detail <i class='bx bx-arrow-back bx-rotate-180' ></i></a> -->
+                </div>
+            </div>
+            <div class="activity">
+            <img src="/assets/images/gambar2.png" class="img-activity" alt="">
+                <div class="activity-description">
+                    <h5 class="title-activity">LOREM IPSUM</h5>
+                    <p class="text-activity">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam</p>
+                </div>
+            </div>
+        </div>
+        <div class="activity-container">
+            <div class="activity">
+            <img src="/assets/images/gambar3.png" class="img-activity" alt="">
+                <div class="activity-description">
+                    <h5 class="title-activity">LOREM IPSUM</h5>
+                    <p class="text-activity">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam</p>
+                </div>
+            </div>
+            <div class="activity">
+            <img src="/assets/images/gambar4.png" class="img-activity" alt="">
+                <div class="activity-description">
+                    <h5 class="title-activity">LOREM IPSUM</h5>
+                    <p class="text-activity">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam</p>
+                </div>
+            </div>
+        </div>
+        </section>
+
+    <!-- KONTAK -->
+    <section class="kontak section" id="kontak">
+        <h2 class="section-title">Kontak Kami</h2>
+        <div class="kontak-container">
+            <div class="kontak-description">
+                <img src="/assets/images/TemukanKami.svg" class="img-kontak" alt="">
+            </div>
+            <div class="kontak-descrption">
+            <div class="sosial-media">
+                <img src="/assets/images/ig.svg" class="img-kontak-media" alt="">
+                Peergroup.id
+            </div>
+            <div class="sosial-media">
+                <img src="/assets/images/mail.svg" class="img-kontak-media" alt="">
+                peergroup.id@gmail.com
+            </div>
+            <div class="sosial-media">
+                <img src="/assets/images/linkedin.svg" class="img-kontak-media" alt="">
+                Peer Group ID
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <!-- <a href="#" class="nav__logo"><img src="/assets/images/Logo Peer Group Transparan.png" alt=""></a>  -->
+        <small>Copyright &copy; Peergroup.id 2021</small>
+    </footer>
+
     </main>
     <!--GASP-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
