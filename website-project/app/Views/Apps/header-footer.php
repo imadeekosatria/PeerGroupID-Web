685c831c1684cdecd4819ceffd08d920455777a8
imadeekosatria <imadeekosatriawiguna@gmail.com>
2021-11-04 14:13:36 +0800

    fix drop down menu (CSS conflict)

--- website-project/app/Views/Apps/header-footer.php
+++ website-project/app/Views/Apps/header-footer.php
@@ -29,11 +29,11 @@
                     <li class="nav__item"><a href="/" class="nav__link">Beranda</a></li>
                     <li class="nav__item"><a href="about" class="nav__link">Siapa Kita</a></li>
                     <li class="nav__item dropdown" >
-                        <a href="/artikel" class="nav__link dropdown__link" id="dropdown">Artikel <i class='bx bx-chevron-down dropdown__icon' id="drop-icon"></i></a>
+                        <a href="#" class="nav__link dropdown__link" id="dropdown">Artikel <i class='bx bx-chevron-down dropdown__icon' id="drop-icon"></i></a>
                         <ul class="dropdown__menu" id="dropdown-menu">
-                            <li class="dropdown__item"><a href="/self-dev" class="nav__link">Self Development</a></li>
-                            <li class="dropdown__item"><a href="/jurusan" class="nav__link">Jurusan</a></li>
-                            <li class="dropdown__item"><a href="/karir" class="nav__link">Karir</a></li>
+                            <li class="dropdown__item"><a href="#" class="nav__link">Self Development</a></li>
+                            <li class="dropdown__item"><a href="#" class="nav__link">Jurusan</a></li>
+                            <li class="dropdown__item"><a href="#" class="nav__link">Karir</a></li>
                         </ul>
                     </li>
                     <li class="nav__item"><a href="#" class="nav__link">Kegiatan Kami</a></li>
